c85704038f911eb2078de21f893d1a6cc2e02e3c
gjmmaster <gabriel.jmmaster@gmail.com>
2025-09-04 18:05:35 -0300

    Update specs.clj

--- src/juridico/api/specs.clj
+++ src/juridico/api/specs.clj
@@ -2,10 +2,10 @@
   (:require [clojure.spec.alpha :as s]))
 
 ;; Spec para garantir que o número do processo é uma string não vazia
-(s/def ::case_number (s/and string? not-empty?))
+(s/def ::case_number (s/and string? not-empty)) ; <-- CORRIGIDO AQUI
 
 ;; Spec para garantir que a jurisdição é uma string não vazia
-(s/def ::jurisdiction (s/and string? not-empty?))
+(s/def ::jurisdiction (s/and string? not-empty)) ; <-- E AQUI TAMBÉM
 
 ;; Spec que define a estrutura do corpo da requisição para criar um processo
 (s/def ::create-process-payload (s/keys :req-un [::case_number ::jurisdiction]))
