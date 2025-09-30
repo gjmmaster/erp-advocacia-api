477e4395ea39dcd7a79e85891495ac67293388b0
gjmmaster <gabriel.jmmaster@gmail.com>
2025-09-30 14:17:47 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 
-  // --- ADIÇÃO CRÍTICA PARA DEPLOY ---
-  // Força o Vite a usar caminhos de asset relativos no build final.
-  // Isso resolve problemas de "tela em branco" em ambientes de produção
-  // ao garantir que o index.html consiga encontrar os arquivos JS e CSS.
-  base: './' 
 })
