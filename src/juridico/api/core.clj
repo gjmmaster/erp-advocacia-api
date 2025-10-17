9323ee78a45b1c65a0496ca64af51af44dced673
Developer <dev@advocacia.com>
2025-10-17 18:16:28 -0300

    fix: adicionar wrap-public-db-repo nas rotas de tenants - Corrige NullPointerException em listar-tenants - Garante que db-repo esta disponivel nos handlers - Corrige erro 401 no frontend

--- src/juridico/api/core.clj
+++ src/juridico/api/core.clj
@@ -20,11 +20,13 @@
     ["/provision-tenant" {:middleware [mw/wrap-jwt-authentication
                                        mw/wrap-super-admin-authorization]
                           :post {:handler h/provision-tenant-handler}}]
-    ["/tenants" {:middleware [mw/wrap-jwt-authentication
+    ["/tenants" {:middleware [mw/wrap-public-db-repo
+                              mw/wrap-jwt-authentication
                               mw/wrap-super-admin-authorization]
                  :get {:handler h/listar-tenants-handler}
                  :post {:handler h/criar-tenant-handler}}]
-    ["/tenants/{id}" {:middleware [mw/wrap-jwt-authentication
+    ["/tenants/{id}" {:middleware [mw/wrap-public-db-repo
+                                   mw/wrap-jwt-authentication
                                    mw/wrap-super-admin-authorization]
                       :get {:handler h/obter-tenant-handler}
                       :put {:handler h/atualizar-tenant-handler}
