1f42ae58f8fb226d32ad1f283d080fa9fcaabdaa
Developer <dev@advocacia.com>
2025-10-31 12:51:43 -0300

    fix: adicionar wrap-public-db-repo nas rotas de impersonation
    - Handlers precisam de db-repo injetado
    - Corrige erro de rota não encontrada
    - Permite impersonation funcionar

--- src/juridico/api/core.clj
+++ src/juridico/api/core.clj
@@ -51,10 +51,12 @@
                                                       mw/wrap-super-admin-authorization]
                                          :get {:handler h/get-tenant-master-user-handler}}]
     ;; Rotas de Impersonation
-    ["/impersonate/{user-id}" {:middleware [mw/wrap-jwt-authentication
+    ["/impersonate/{user-id}" {:middleware [mw/wrap-public-db-repo
+                                            mw/wrap-jwt-authentication
                                             mw/wrap-super-admin-authorization]
                                :post {:handler h/start-impersonation-handler}}]
-    ["/stop-impersonate" {:middleware [mw/wrap-jwt-authentication]
+    ["/stop-impersonate" {:middleware [mw/wrap-public-db-repo
+                                       mw/wrap-jwt-authentication]
                           :post {:handler h/stop-impersonation-handler}}]]
    ["/auth" {:middleware [mw/wrap-public-db-repo mw/wrap-tenant-context]}
     ["/login" {:post {:handler h/login-handler}}]]
